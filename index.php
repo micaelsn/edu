69f3e98c62bf79e9dc8fb840c9d5ed8ef76726e6
micael <micaelsn>
2018-11-06 17:21:31 -0300

    Páginas index e login com bootstrap

--- index.php
+++ index.php
@@ -5,16 +5,102 @@
 		<html>
 			<head>
 				<link rel="stylesheet" type="text/css" href="css/style.css">
-				<title>Login</title>
+				<link rel="stylesheet" type="text/css" href="css/indexStyle.css">
+				<title>Página Inicial</title>
+				<!-- Bootstrap Core CSS -->
+				<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+				
 			</head>
 			<body>';
 		echo '
-			<section>
-				<p>Aviso: página apenas para a coordenação</p>
-				<p><a href="login.php">Faça seu Login</a></p>
-			</section>';
+		<!-- Navigation -->
+		<nav class="navbar navbar-expand-lg navbar-light bg-light fixed-top">
+		  <div class="container">
+			<a class="navbar-brand" href="index.php">Education Foundation</a>
+			<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+				  <span class="navbar-toggler-icon"></span>
+				</button>
+			<div class="collapse navbar-collapse" id="navbarResponsive">
+			  <ul class="navbar-nav ml-auto">
+				<li class="nav-item active">
+				  <a class="nav-link" href="#">Home
+						<span class="sr-only">(current)</span>
+					  </a>
+				</li>
+				<li class="nav-item">
+				  <a class="nav-link" href="#">About</a>
+				</li>
+				<li class="nav-item">
+				  <a class="nav-link" href="#">Services</a>
+				</li>
+				<li class="nav-item">
+				  <a class="nav-link" href="#">Contact</a>
+				</li>
+				<li class="nav-item">
+					<a class="nav-link" href="login.php">Faça seu Login</a>
+				</li>
+			  </ul>
+			</div>
+		  </div>
+		</nav>
+		
+		<header>
+		  <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+			<ol class="carousel-indicators">
+			  <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+			  <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+			  <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+			</ol>
+			<div class="carousel-inner" role="listbox">
+			  <!-- Slide One - Set the background image for this slide in the line below -->
+			  <div class="carousel-item active" style="background-image: url(\'https://images.pexels.com/photos/267885/pexels-photo-267885.jpeg?cs=srgb&dl=accomplishment-celebrate-ceremony-267885.jpg&fm=jpg\')">
+				<div class="carousel-caption d-none d-md-block">
+				  <h2 class="display-4">First Slide</h2>
+				  <p class="lead">This is a description for the first slide.</p>
+				</div>
+			  </div>
+			  <!-- Slide Two - Set the background image for this slide in the line below -->
+			  <div class="carousel-item" style="background-image: url(\'https://images.pexels.com/photos/256541/pexels-photo-256541.jpeg?cs=srgb&dl=bookcase-books-bookshelves-256541.jpg&fm=jpg\')">
+				<div class="carousel-caption d-none d-md-block">
+				  <h2 class="display-4">Second Slide</h2>
+				  <p class="lead">This is a description for the second slide.</p>
+				</div>
+			  </div>
+			  <!-- Slide Three - Set the background image for this slide in the line below -->
+			  <div class="carousel-item" style="background-image: url(\'https://images.pexels.com/photos/1181398/pexels-photo-1181398.jpeg?cs=srgb&dl=cabinet-discussing-facial-expression-1181398.jpg&fm=jpg\')">
+				<div class="carousel-caption d-none d-md-block">
+				  <h2 class="display-4">Third Slide</h2>
+				  <p class="lead">This is a description for the third slide.</p>
+				</div>
+			  </div>
+			</div>
+			<a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+				  <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+				  <span class="sr-only">Previous</span>
+				</a>
+			<a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+				  <span class="carousel-control-next-icon" aria-hidden="true"></span>
+				  <span class="sr-only">Next</span>
+				</a>
+		  </div>
+		</header>
+		
+		<!-- Page Content -->
+		<section class="py-5">
+		  <div class="container">
+			<h1 class="display-4">Full Page Image Slider</h1>
+			<p class="lead">The background images for the slider are set directly in the HTML using inline CSS. The images in this snippet are from <a href="https://unsplash.com">Unsplash</a>, taken by <a href="https://unsplash.com/@joannakosinska">Joanna Kosinska</a>!</p>
+		  </div>
+		</section>
+';
 			
 	echo '
+			<!-- Bootstrap core JavaScript -->
+			<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+			<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+			<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+
 			</body>
+
 		</html>';
 ?>
--- login.php
+++ login.php
@@ -8,23 +8,101 @@
 	<!DOCTYPE html>
 		<html>
 			<head>
-				<link rel="stylesheet" type="text/css" href="css/style.css">
+				<link rel="stylesheet" type="text/css" href="css/styleLogin.css">
+				<!-- Bootstrap Core CSS -->
+				<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
 				<title>Login</title>
 			</head>
 			<body>';
-				//O método GET deixa os dados expostos na url, por isso para se ter mais segura é recomendado usar POST
-				if (!isset($_REQUEST['submeter'])) {
-					echo '
-						<section>
-							<form id="meuForm" method="post" action="main.php">
-								<h2>Login</h2>
-								Username: <input type="text" name="txt_username" autofocus="true"><br><br>
-								Password: <input type="password" name="txt_password"><br><br>
-								<input id="submeter" type="submit" value="Enter" name="btn_submeter"><br><br>
-							</form>
-						</section>';
-				}
+			echo '
+				<!-- Navigation -->
+				<nav class="navbar navbar-expand-lg navbar-light bg-light fixed-top">
+					<div class="container">
+					<a class="navbar-brand" href="index.php">Education Foundation</a>
+					<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+							<span class="navbar-toggler-icon"></span>
+						</button>
+					<div class="collapse navbar-collapse" id="navbarResponsive">
+						<ul class="navbar-nav ml-auto">
+						<li class="nav-item">
+							<a class="nav-link" href="index.php">Home</a>
+						</li>
+						<li class="nav-item">
+							<a class="nav-link" href="#">About</a>
+						</li>
+						<li class="nav-item">
+							<a class="nav-link" href="#">Services</a>
+						</li>
+						<li class="nav-item">
+							<a class="nav-link" href="#">Contact</a>
+						</li>
+						<li class="nav-item active">
+							<a class="nav-link" href="login.php">Faça seu Login
+							<span class="sr-only">(current)</span>
+							</a>
+						</li>
+						</ul>
+					</div>
+					</div>
+				</nav><br>/<br/><br/><br/>';
+			//O método GET deixa os dados expostos na url, por isso para se ter mais segura é recomendado usar POST
+			if (!isset($_REQUEST['submeter'])) {
+				echo '
+				<section>
+				<div class="container">
+					<div class="d-flex justify-content-center h-100">
+						<div class="card">
+							<div class="card-header">
+								<h3>Sign In</h3>
+							</div>
+							<div class="card-body">
+								<form id="meuForm" method="post" action="main.php">
+									<div class="input-group form-group">
+										<div class="input-group-prepend">
+											<span class="input-group-text"><i class="fas fa-user"></i></span>
+										</div>
+										<input type="text" class="form-control" placeholder="username" name="txt_username">
+										
+									</div>
+									<div class="input-group form-group">
+										<div class="input-group-prepend">
+											<span class="input-group-text"><i class="fas fa-key"></i></span>
+										</div>
+										<input type="password" class="form-control" placeholder="password" name="txt_password">
+									</div>
+									<div class="row align-items-center remember">
+										<input type="checkbox">Remember Me
+									</div>
+									<div class="form-group">
+										<input type="submit" value="Login" class="btn float-right login_btn" name="btn_submeter">
+									</div>
+								</form>
+							</div>
+							<div class="card-footer">
+								<div class="d-flex justify-content-center links">
+									Don\'t have an account?<a href="#">Sign Up</a>
+								</div>
+								<div class="d-flex justify-content-center">
+									<a href="#">Forgot your password?</a>
+								</div>
+							</div>
+						</div>
+					</div>
+					</div>
+						<!--form id="meuForm" method="post" action="main.php">
+							<h2>Login</h2>
+							Username: <input type="text" name="txt_username" autofocus="true"><br><br>
+							Password: <input type="password" name="txt_password"><br><br>
+							<input id="submeter" type="submit" value="Enter" name="btn_submeter"><br><br>
+						</form-->
+					</section>';
+			}
 	echo '
+			<!-- Bootstrap core JavaScript -->
+			<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+			<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+			<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+
 			</body>
 		</html>';
 ?>
